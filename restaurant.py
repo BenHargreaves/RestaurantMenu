46605428fccb83db995175ab0f07eb416375ce64
Ben Hargreaves <bhargreaves91@gmail.com>
2018-05-14 18:12:25 -0400

    updated restaurant listings

--- restaurant.py
+++ restaurant.py
@@ -8,16 +8,27 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-myFirstRestaurant = Restaurant(name = 'Pizza Palace')
-session.add(myFirstRestaurant)
-session.commit()
-session.query(Restaurant).all()
+#myFirstRestaurant = Restaurant(name = 'Pizza Palace')
+#session.add(myFirstRestaurant)
+#session.commit()
+#restaurants = session.query(Restaurant).all()
 
-cheesepizza = MenuItem(name = "Cheese Pizza", description = "Made with Cheese, duh",
-    course = "Entree", price = "$8.99", restaurant = myFirstRestaurant)
-session.add(cheesepizza)
-session.commit()
-firstResult = session.query(MenuItem).first()
+#for restaurant in restaurants:
+#    print(restaurant)
 
-print(firstResult.name)
-print(session.query(Restaurant).all())
+#cheesepizza = MenuItem(name = "Cheese Pizza", description = "Made with Cheese, duh",
+#    course = "Entree", price = "$8.99", restaurant = myFirstRestaurant)
+#session.add(cheesepizza)
+#session.commit()
+#firstResult = session.query(MenuItem).first()
+
+#print(firstResult.name)
+#print(session.query(Restaurant).all())
+
+VeggieBurgers = session.query(MenuItem).filter_by(name = 'Veggie Burger')
+
+for veggieBurger in VeggieBurgers:
+    print(veggieBurger.id)
+    print(veggieBurger.price)
+    print(veggieBurger.restaurant.name)
+    print('\n')
